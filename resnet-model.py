72fd4049bbafa0fbb150632caa1bf2aade3841f4
dudderThugger <88734852+dudderThugger@users.noreply.github.com>
2024-05-30 18:06:29 +0200

    pres done

--- resnet-model.py
+++ resnet-model.py
@@ -7,12 +7,13 @@ from tensorflow import keras
 ## Keras library also provides ResNet101V2 and ResNet50V2. Import them and use it for other experiments.
 from tensorflow.keras.applications import ResNet152V2
 from tensorflow.keras.applications import ResNet50V2
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.callbacks import ReduceLROnPlateau
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras import metrics
 import time
 import numpy as np
 import pandas as pd
+from tensorflow.keras.models import model_from_json
 from keras import backend as K
 
 # Check CUDA functionality, restart kernel to change GPUs
@@ -38,7 +39,8 @@ buffersize = 2
 im_dim_x = 224
 im_dim_y = 224
 batchSizeIntInitial = 10
-batchSizeInt = 6
+batchSizeInt = 32
+model_name = "resnet50_with_batch_norm_extendedV3"
 
 train = tf.keras.preprocessing.image_dataset_from_directory(
     traindir, image_size=(im_dim_x, im_dim_y), batch_size=batchSizeInt)
@@ -62,7 +64,8 @@ mirrored_strategy = tf.distribute.MirroredStrategy()
 
 with mirrored_strategy.scope():  # the entire model needs to be compiled within the scope of the distribution strategy
     # cb1 = EarlyStopping(monitor='val_accuracy', patience=4)  # define early stopping callback function
-    cb1 = ModelCheckpoint("/mnt/d/datasets/INBREAST/results/resnet50.h5", monitor="val_accuracy", verbose=2, save_best_only=True, mode="max")
+    cb1 = ModelCheckpoint(f"/mnt/d/datasets/INBREAST/results/{model_name}.keras", monitor="val_accuracy", verbose=2, save_best_only=True, mode="max")
+    cb2 = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=0.000001)
     # cb2 = ReduceLROnPlateau(monitor='val_accuracy', factor=0.2, patience=2,
     #                         min_lr=0.00001)  # define LR reduction callback function
     opt = keras.optimizers.Adam(learning_rate=lr)
@@ -76,22 +79,21 @@ with mirrored_strategy.scope():  # the entire model needs to be compiled within 
     ptmodel.layers[-1].trainable = True
 
 
-    # un-freeze the BatchNorm layers
+    #un-freeze the BatchNorm layers
     # for layer in ptmodel.layers:
     #     if "BatchNormalization" in layer.__class__.__name__:
-    #         layer.trainable = False # ??
+    #         layer.trainable = False
 
     last_output = ptmodel.output
     x = tf.keras.layers.Flatten()(last_output)
-    # x = tf.keras.layers.Dense(2048, activation='relu')(x)
-    # #     x = tf.keras.layers.Dropout(0.15)(x)
-    # x = tf.keras.layers.Dense(1024, activation='relu')(x)
-    # #    x = tf.keras.layers.Dropout(0.3)(x)
-    x = tf.keras.layers.Dense(512, activation='relu')(x)
+    x = tf.keras.layers.Dense(1024, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    x = tf.keras.layers.Dense(256, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
     x = tf.keras.layers.Dense(128, activation='relu')(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    x = tf.keras.layers.Dense(64, activation='relu')(x)
     x = tf.keras.layers.Dense(32, activation='relu')(x)
-    # # x = tf.keras.layers.Dropout(0.5, seed=34)(x)
-    # x = tf.keras.layers.Dense(64, activation = 'relu')(x)
     x = tf.keras.layers.Dense(1, activation='sigmoid')(x)
     model = tf.keras.Model(ptmodel.input, x)
     model.compile(optimizer=opt,
@@ -100,16 +102,18 @@ with mirrored_strategy.scope():  # the entire model needs to be compiled within 
 
 print("---time taken : %s seconds ---" % (time.time() - start_time))
 # Train model
-history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[cb1])
+model.summary()
+history = model.fit(train_ds, validation_data=val_ds, epochs=epochs, callbacks=[cb1, cb2])
 print("---time taken : %s seconds ---" % (time.time() - start_time))
 
 # Save training subresults to csv
 history_df = pd.DataFrame(history.history)
-history_df.to_csv("/mnt/d/datasets/INBREAST/results/resnet50_history.csv", index=False)
+history_df.to_csv(f"/mnt/d/datasets/INBREAST/results/{model_name}_history.csv", index=False)
+
+model = tf.keras.models.load_model(f"/mnt/d/datasets/INBREAST/results/{model_name}.keras")
 
 # Test model
 # Loading checkpoint model
-model.load_weights("/mnt/d/datasets/INBREAST/results/resnet50.h5")
 testloss, testaccuracy, testauc, precision, recall = model.evaluate(test_ds)
 print('Test accuracy :', testaccuracy)
 print('Test AUC :', testauc)
@@ -119,12 +123,16 @@ print('Test F1 :', F1)
 print('Test precision :', precision)
 print('Test recall :', recall)
 
-# Model path setup.
-if not os.path.exists("saved_model_resnet50_simple" + dirName):
-    os.makedirs("saved_model_resnet50_simple" + dirName + '/')
+test_results = {'Accuracy': testaccuracy, 'Test AUC': testauc, 'F1': F1, 'precision': precision, 'recall': recall}
+results_df = pd.DataFrame(test_results)
 
-model.save('saved_model_resnet50_simple' + dirName + '/resnet152v2_1')
+results_df.to_csv(f"/mnt/d/datasets/EMBED/results/{model_name}_results.csv")
+
+tf.keras.models.save_model(model, f'{model_name}.h5')
+loaded_model = tf.keras.models.load_model(f'{model_name}.h5')
+
 predicted_probs = []
+loaded_probs = []
 true_classes = []
 
 for images, labels in test_ds:
@@ -134,10 +142,18 @@ for images, labels in test_ds:
     predicted_probs.extend(predicted_prob)
     true_classes.extend(true_class)
 
+    loaded_prob = loaded_model(images).numpy()
+
+    loaded_probs.extend(predicted_prob)
+
+print(predicted_probs)
+print(loaded_probs)
+
 probs_dict = {
     'Probabilities': predicted_probs,
     'Labels': true_classes
 }
 
 probs_df = pd.DataFrame(probs_dict)
-probs_df.to_csv("/mnt/d/datasets/INBREAST/results/resnet50_probs.csv", index=False)
+probs_df.to_csv(f"/mnt/d/datasets/EMBED/results/{model_name}_probs.csv", index=False)
+
